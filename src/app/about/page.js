5d0b9c6d4047ce8d900afd484fde7bdfadedbbcd
krrrrrg <megaryyy@naver.com>
2025-03-09 15:39:20 +0900

    Footer 컴포넌트 경로 수정

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function About() {
   return (
