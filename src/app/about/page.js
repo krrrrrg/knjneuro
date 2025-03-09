84891375b710a68bb35cc45fdaf0d3ce6d42d271
krrrrrg <megaryyy@naver.com>
2025-03-09 15:34:55 +0900

    ㄴㄴ

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import Navbar from "../components/Navbar";
-import Footer from "올바른/경로/Footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function About() {
   return (
